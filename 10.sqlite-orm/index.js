fcef2f4e4a31de1bff3a094df823cc465a39cc64
he55 <40764272+he55@users.noreply.github.com>
2024-03-31 02:28:51 +0800

    Update index.js

--- 10.sqlite-orm/index.js
+++ 10.sqlite-orm/index.js
@@ -1,39 +1,28 @@
 import { Sequelize, DataTypes } from 'sequelize';
 
-const sequelize = new Sequelize({
-    dialect:'sqlite',
-    storage:'db.sqlite'
-});
+const sequelize = new Sequelize('sqlite::memory:');
 
 try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-  
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
 const User = sequelize.define('User', {
   username: DataTypes.STRING,
   birthday: DataTypes.DATE,
 });
 
+// Automatically create all tables
+await sequelize.sync();
+
 const jane = await User.create({
-    username: 'janedoe',
-    birthday: new Date(1980, 6, 20),
-  });
-  
-  const users = await User.findAll();
-  console.log(users)
-  debugger
+  username: 'janedoe',
+  birthday: new Date(1980, 6, 20),
+});
 
-
-(async function(){
-
-    try {
-       
-    } catch (error) {
-        console.log(error)
-    }
-   
-    
-})()
+const users = await User.findAll();
+if(users.length){
+  console.log(users[0].username,users[0].birthday)
+}
