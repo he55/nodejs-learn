b2dce1e72f31ffa3b1e418e974c08d337731002c
he55 <40764272+he55@users.noreply.github.com>
2024-04-20 22:43:31 +0800

    14.threejs

---
+++ 14.threejs/src/main.ts
@@ -0,0 +1,34 @@
+import * as THREE from 'three';
+import './style.css'
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const light = new THREE.AmbientLight(0xffffff, 30)
+scene.add(light)
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshPhongMaterial({ color: 0x404040, map: new THREE.TextureLoader().load('winking_face_with_tongue_3d.png') });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame(animate);
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render(scene, camera);
+}
+
+animate();
+
+window.addEventListener('wheel', (e) => {
+	camera.position.z += e.deltaY / 100 * 0.1
+})
