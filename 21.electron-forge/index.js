81bfccd05357c39a7619a5232468719c317d915c
he55 <40764272+he55@users.noreply.github.com>
2024-05-22 23:16:19 +0800

    electron-forge

---
+++ 21.electron-forge/index.js
@@ -0,0 +1,13 @@
+const { BrowserWindow, app } = require("electron");
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width:400,
+        height:300,
+    })
+    mainWindow.loadFile('index.html')
+}
+
+app.whenReady().then(()=>{
+    createWindow()
+})
