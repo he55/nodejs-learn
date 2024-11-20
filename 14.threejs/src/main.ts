0e7dbb53e275e2ac855505b4be4d43df1868974a
he55 <40764272+he55@users.noreply.github.com>
2024-11-20 22:44:40 +0800

    fix

--- 14.threejs/src/main.ts
+++ 14.threejs/src/main.ts
@@ -5,6 +5,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -16,7 +17,7 @@ const material = new THREE.MeshPhongMaterial({ color: 0x404040, map: new THREE.T
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 2;
 
 function animate() {
 	requestAnimationFrame(animate);
